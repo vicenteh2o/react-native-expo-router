175d42cbe6e541157385dfb46edd9518fb6de2c6
vicenteh2o <vicentefleitas@Vicentes-MacBook-Pro.local>
2025-08-29 16:18:19 -0300

    feat: simple formik yup implementation

--- app/auth/index.tsx
+++ app/auth/index.tsx
@@ -1,11 +1,102 @@
-import { ScrollView, StyleSheet, Text } from "react-native";
+import { Formik, FormikHelpers } from "formik";
+import { useState } from "react";
+import {
+  ActivityIndicator,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { User } from "../shared/entities/user";
+import { validationSchema } from "./schema/validationSchema";
 
 const Auth = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const handleLogIn = (
+    values: { email: string; password: string },
+    actions: FormikHelpers<{ email: string; password: string }>
+  ) => {
+    console.log("submit: ", values);
+    setUser({ id: "1", name: "John Doe", email: values.email });
+    setTimeout(() => {
+      console.log(values);
+      actions.setSubmitting(false); // stop loading
+      actions.resetForm();
+    }, 2000);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <Text>Auth</Text>
+      <View style={{ alignItems: "center" }}>
+        <Text style={styles.title}>Autenticación</Text>
+      </View>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={validationSchema}
+          onSubmit={handleLogIn}
+        >
+          {({
+            handleChange,
+            values,
+            handleBlur,
+            errors,
+            handleSubmit,
+            isSubmitting,
+          }) => (
+            <View
+              style={{
+                justifyContent: "center",
+                backgroundColor: "#fff",
+                flex: 1,
+                padding: 16,
+                height: "100%",
+              }}
+            >
+              <View style={styles.inputWrapper}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="email"
+                  value={values.email}
+                  onChangeText={handleChange("email")}
+                  onBlur={handleBlur("email")}
+                />
+                {errors.email && (
+                  <Text style={styles.error}>{errors.email}</Text>
+                )}
+              </View>
+              <View style={styles.inputWrapper}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="password"
+                  value={values.password}
+                  onChangeText={handleChange("password")}
+                  onBlur={handleBlur("password")}
+                />
+                {errors.password && (
+                  <Text style={styles.error}>{errors.password}</Text>
+                )}
+              </View>
+              <View style={styles.inputWrapper}>
+                <TouchableOpacity
+                  style={styles.loginButton}
+                  onPress={() => handleSubmit()}
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? (
+                    <ActivityIndicator color="#fff" />
+                  ) : (
+                    <Text style={styles.loginButtonText}>LogIn</Text>
+                  )}
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
+        </Formik>
       </ScrollView>
     </SafeAreaView>
   );
@@ -15,13 +106,44 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
+    marginTop: 10,
+  },
+  inputWrapper: {
+    width: "100%",
+    paddingHorizontal: 16,
+  },
+  input: {
+    width: "100%",
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    marginVertical: 8,
+    backgroundColor: "#fff",
+  },
+  error: {
+    color: "#ff4500",
+  },
+  loginButton: {
+    backgroundColor: "#007AFF",
+    paddingVertical: 12,
+    borderRadius: 8,
     alignItems: "center",
+    marginVertical: 16,
+    width: "100%",
+  },
+  loginButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    letterSpacing: 1,
   },
 });
 
 export default Auth;
-
-// Expo Router: Oculta el header automático para esta pantalla
-export const options = {
-  headerShown: false,
-};
